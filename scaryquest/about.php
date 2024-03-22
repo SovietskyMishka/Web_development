3424735e66958fa152f58c5c5c41953ebdc00f11
SovietskyMishka <164509770+SovietskyMishka@users.noreply.github.com>
2024-03-22 17:10:37 +0300

    Add files via upload

---
+++ scaryquest/about.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: about
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="content-block-dark">
+            <h1 id="head-block">О нас</h1>
+        </div>
+
+<?php get_footer(); ?>
